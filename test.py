1f7d24615cbbf82c9198fae32463d4b17d844864
sawan201 <160764319+sawan201@users.noreply.github.com>
2026-02-25 10:44:22 -0600

    Refactor coercion checks and change R to QQ
    Updated coercion checks and modified variable R to use QQ for better compatibility.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from sage.all import *
 from MacMahon import MacMahonSymmetricFunctions
 from vector_partition_ahmad import VectorPartition, VectorPartitions
-R = QQ
+R = QQ #Using QQ instead of ZZ because basis change formulas will sometimes produce coefficients in QQ which cannot be coerced
 A = MacMahonSymmetricFunctions(R)
 
 P = A.P()
@@ -18,20 +18,23 @@ h = H[lam]
 
 # 1) Does Sage think a coercion exists
 print("M <- P ?", M.has_coerce_map_from(P))
-print("P <- M ?", P.has_coerce_map_from(M))
+print("M -> P ?", P.has_coerce_map_from(M))
 print("P <- E ?", P.has_coerce_map_from(E))
+print("P -> E ?", E.has_coerce_map_from(P))
 print("P <- H ?", P.has_coerce_map_from(H))
+print("P -> H ?", H.has_coerce_map_from(P))
 
 # 2) What is the actual map object
 print("M.coerce_map_from(P):", M.coerce_map_from(P))
 print("P.coerce_map_from(M):", P.coerce_map_from(M))
 print("P.coerce_map_from(E):", P.coerce_map_from(E))
+print("E.coerce_map_from(P):", E.coerce_map_from(P))
 print("P.coerce_map_from(H):", P.coerce_map_from(H))
 
 # 3) Explicit coercion on elements
 print("M(p) =", M(p))     # coercion P -> M
 print("P(m) =", P(m))     # coercion M -> P
-print("P(e) =", P(e))     # coercion E -> P
+print("P(e) =", P(e))     # coercion E -> P, P -> E will crash because no map exists from E -> anywhere currently
 print("P(h) =", P(h))     # coercion H -> P
 print("H(p) =", H(p))     # coercion P -> H
 
@@ -46,4 +49,4 @@ print("p * m =", p * m)
 
 # 6) Checking Monomial Multiplication
 
-print("M[1,1] * M[2,2] =", M[1,1] * M[2,2])
+print("M[0,1] * M[1,2] =", M[0,1] * M[1,2])
